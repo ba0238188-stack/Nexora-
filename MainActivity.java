239fc0f420ab3db8415bdec3fc503b590b0fa878
ba0238188-stack <ba0238188@gmail.com>
2026-09-19 02:43:22 +0300

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -72,7 +72,7 @@ public class MainActivity extends Activity {
                 if (fileCallback != null) fileCallback.onReceiveValue(null);
                 fileCallback = cb;
 
-                Intent pick = new Intent(Intent.ACTION_GET_CONTENT);
+                Intent pick = new Intent(Intent.ACTION_OPEN_DOCUMENT);
                 pick.addCategory(Intent.CATEGORY_OPENABLE);
                 pick.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
                 pick.setType("*/*");
