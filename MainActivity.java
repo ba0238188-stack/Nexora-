5a0a9518dcb5748fb004a8ab110cfdca20024ca6
ba0238188-stack <ba0238188@gmail.com>
2026-09-19 03:08:08 +0300

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -15,6 +15,7 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.view.ViewGroup;
+import android.view.View;
 
 import java.util.ArrayList;
 
@@ -27,6 +28,7 @@ public class MainActivity extends Activity {
     @Override public void onCreate(Bundle b) {
         super.onCreate(b);
         webView = new WebView(this);
+        webView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
         setContentView(webView, new ViewGroup.LayoutParams(-1, -1));
         requestNeededPermissions();
         configure();
